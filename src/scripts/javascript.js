36dba57cb59dc9e6f7d4e4682744f342c16aa9dc
petrosAth <develop@athanasoulis.me>
2023-02-12 08:05:46 +0200

    Add show dialog js functionality

---
+++ src/scripts/javascript.js
@@ -0,0 +1,6 @@
+const modal = document.querySelector('.add-dialog');
+const openModal = document.querySelector('.add-button');
+
+openModal.addEventListener('click', () => {
+  modal.showModal();
+});
