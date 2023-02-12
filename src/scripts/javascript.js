6626894d48f86e5a574fea492529dace8033a697
petrosAth <develop@athanasoulis.me>
2023-02-12 19:10:17 +0200

    Add create book HTML function

--- src/scripts/javascript.js
+++ src/scripts/javascript.js
@@ -5,46 +5,54 @@ openModal.addEventListener('click', () => {
   modal.showModal();
 });
 
+function createBookElement(element, classes, info, index) {
+  const newElement = document.createElement(element);
+  classes.forEach((elementClass) => {
+    newElement.classList.add(elementClass);
+  });
+
+  if (info) {
+    newElement.innerText = info;
+  }
+
+  if (index) {
+    newElement.dataset.index = index;
+  }
+
+  return newElement;
+}
+
+function appendChild(element, children) {
+  children.forEach((child) => {
+    element.appendChild(child);
+  });
+}
+
 function CreateBookHTML(index, title, author, pages, is_read) {
-  const bookLabel = document.createElement('div');
-  bookLabel.classList.add('book__label', 'book__label--not-read');
+  const bookLabel = createBookElement('div', [
+    'book__label',
+    is_read ? 'book__label--is-read' : 'book__label--not-read',
+  ]);
+  const bookButtonStatus = createBookElement('div', [
+    'button',
+    'book__button',
+    'book__button-status',
+    is_read ? 'book__button-status--is-read' : 'book__button-status--not-read',
+  ]);
+  const bookAuthor = createBookElement('div', ['book__main__info__author'], author);
+  const bookTitle = createBookElement('div', ['book__main__info__title'], title);
+  const bookPages = createBookElement('div', ['book__main__info__pages'], pages);
 
-  const bookButtonStatus = document.createElement('button');
-  bookButtonStatus.classList.add('button', 'book__button', 'book__button-status', 'book__button-status--not-read');
+  const bookInfo = createBookElement('div', ['book__main__info']);
+  appendChild(bookInfo, [bookAuthor, bookTitle, bookPages]);
 
-  const bookAuthor = Object.assign(document.createElement('div'), {
-    className: 'book__main__info__author',
-    innerText: 'Tolkien',
-  });
-  const bookTitle = Object.assign(document.createElement('div'), {
-    className: 'book__main__info__title',
-    innerText: 'Lord of the rings',
-  });
-  const bookPages = Object.assign(document.createElement('div'), {
-    className: 'book__main__info__pages',
-    innerText: '999',
-  });
+  const bookMain = createBookElement('div', ['book__main']);
+  appendChild(bookMain, [bookInfo]);
 
-  const bookInfo = document.createElement('div');
-  bookInfo.classList.add('book__main__info');
-  bookInfo.appendChild(bookAuthor);
-  bookInfo.appendChild(bookTitle);
-  bookInfo.appendChild(bookPages);
+  const bookButtonDelete = createBookElement('div', ['button', 'book__button', 'book__button-delete']);
 
-  const bookMain = document.createElement('div');
-  bookMain.classList.add('book__main');
-  bookMain.appendChild(bookInfo);
-
-  const bookButtonDelete = document.createElement('button');
-  bookButtonDelete.classList.add('button', 'book__button', 'book__button-delete');
-
-  const book = document.createElement('div');
-  book.appendChild(bookLabel);
-  book.appendChild(bookButtonStatus);
-  book.appendChild(bookMain);
-  book.appendChild(bookButtonDelete);
-  book.classList.add('book');
-  book.dataset.index = 82;
+  const book = createBookElement('div', ['book'], '', index);
+  appendChild(book, [bookLabel, bookButtonStatus, bookMain, bookButtonDelete]);
 
   let bookself = document.querySelector('.bookself');
   bookself.appendChild(book);
